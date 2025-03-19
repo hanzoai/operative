3f8923c3e1080972360e0666f992d1cb2c82c5d6
Hanzo Dev <dev@hanzo.ai>
2025-03-18 23:42:57 -0500

    Optimize prompts

--- operative/loop.py
+++ operative/loop.py
@@ -48,8 +48,9 @@ SYSTEM_PROMPT = f"""<SYSTEM_CAPABILITY>
 * You are utilising an Ubuntu virtual machine using {platform.machine()} architecture with internet access.
 * You can feel free to install Ubuntu applications with your bash tool, make sure to use scripting friendly commands like apt-get and apt-cache.
 * Use curl instead of wget.
-* To open firefox, use bash tool and run `DISPLAY=:1 firefox-esr & disown`.
+* Replace google.com link with relevant link if user requests different.
 * Using bash tool you can start GUI applications, but you need to set export DISPLAY=:1 and use a subshell. For example "(DISPLAY=:1 xterm &)". GUI apps run with bash tool will appear within your desktop environment, but they may take some time to appear. Take a screenshot to confirm it did.
+* To open firefox, use bash tool and run `DISPLAY=:1 firefox-esr https://google.com & disown`.
 * When using your bash tool with commands that are expected to output very large quantities of text, redirect into a tmp file and use str_replace_editor or `grep -n -B <lines before> -A <lines after> <query> <filename>` to confirm output.
 * When viewing a page it can be helpful to zoom out so that you can see everything on the page.  Either that, or make sure you scroll down to see everything before deciding something isn't available.
 * When using your computer function calls, they take a while to run and send back to you.  Where possible/feasible, try to chain multiple of these calls all into one function calls request.
@@ -57,6 +58,7 @@ SYSTEM_PROMPT = f"""<SYSTEM_CAPABILITY>
 </SYSTEM_CAPABILITY>
 
 <IMPORTANT>
+* When you need to search for something, use default search options, remember you can search on Google directly from Firefox address bar.
 * If the item you are looking at is a pdf, if after taking a single screenshot of the pdf it seems that you want to read the entire document instead of trying to continue to read the pdf from your screenshots + navigation, determine the URL, use curl to download the pdf, install and use pdftotext to convert it to a text file, and then read that text file directly with your StrReplaceEditTool.
 </IMPORTANT>"""
 
